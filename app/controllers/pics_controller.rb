7e398a6605bbc20c13b1b182111d9a455abe3b40
Kevin <kev@apples-MacBook-Air.local>
2013-09-14 20:58:38 -0400

    Add db connection to views and controllers

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,26 +1,35 @@
 class PicsController < ApplicationController
 
+
 	def new
-		
 		@pic = Pic.new	
-
-
 	end
+
+
 
 	def index
+		@pics = Pic.all
 	end
 
+
+
 	def create
+		@pic = Pic.create(params[:pic])
+		redirect_to pics_path
 	end
+
 
 	def show
 	end
 
+
 	def destroy
 	end
 
+
 	def edit
 	end
+
 
 	def update
 	end
